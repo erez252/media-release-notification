c9d88a9a35de2ce2aa1c760dbfbd91760c530a9c
erez252 <mishorierez@gmail.com>
2026-09-23 18:57:25 +0300

    Update get-data.py

--- api/get-data.py
+++ api/get-data.py
@@ -14,13 +14,11 @@ header = {
 
 class handler(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
-    self.send_response(200)
-    self.send_header('Access-Control-Allow-Origin', '*')
-    self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    self.send_header(
-        'Access-Control-Allow-Headers', 'Content-Type, Authorization'
-    )
-    self.end_headers()
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        self.end_headers()
     def do_GET(self):
         
 
